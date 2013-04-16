78aae41025cd52909fece15e3e1735adbe960857
ostsjoe <joeoneill@gmail.com>
2013-04-16 16:20:41 +0000

    Fixed metadata files

--- chz-firewall/metadata.rb
+++ chz-firewall/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Cheezburger Inc"
 maintainer_email "joey@cheezburger.com"
-license          "All rights reserved"
+license          "New BSD License"
 description      "Installs/Configures firewalls"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
