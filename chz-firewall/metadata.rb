96911829c74041f3190a30991f753ad84a36d6e8
ostsjoe <joeoneill@gmail.com>
2015-02-11 21:39:32 +0000

    Version up

--- chz-firewall/metadata.rb
+++ chz-firewall/metadata.rb
@@ -3,5 +3,4 @@ maintainer_email "joey@cheezburger.com"
 license          "New BSD License"
 description      "Installs/Configures firewalls"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
-depends		 "powershell"
+version          "0.2.2"
