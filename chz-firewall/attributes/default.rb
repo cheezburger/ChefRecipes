1bb95fbb0ebfed033839ef65184721e1b58a3d1b
ostsjoe <joeoneill@gmail.com>
2015-02-11 21:39:53 +0000

    no ports open by default

--- chz-firewall/attributes/default.rb
+++ chz-firewall/attributes/default.rb
@@ -13,7 +13,7 @@ default['chz-firewall']['iptables']['savefile']	= "/etc/chz-iptables.save"
 case node['platform_family']
 when "debian","rhel","fedora","suse"
   default['chz-firewall']['firewall_type'] 	= "iptables"
-  default['chz-firewall']['tcp_in']           	= [ 22 ]
+  default['chz-firewall']['tcp_in']           	= [ ]
   default['chz-firewall']['tcp_out']            = [ 20,21,22,25,53,80,110,113,443,4740 ]
   default['chz-firewall']['udp_in']          	= [ ]
   default['chz-firewall']['udp_out']            = [ 20,21,53,113,123 ]
