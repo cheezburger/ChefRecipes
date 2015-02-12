b99dc195f4398f95a4fc1217aaeefb042c8b9ddd
ostsjoe <joeoneill@gmail.com>
2015-02-12 00:20:03 +0000

    adding supported platforms

--- chz-firewall/metadata.rb
+++ chz-firewall/metadata.rb
@@ -5,3 +5,5 @@ license          "New BSD License"
 description      "Installs/Configures firewalls"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.2"
+supports         "ubuntu"
+supports         "windows"
