eda15f942f3a07b8eb0bc0500c01725320c4e1ee
ostsjoe <joeoneill@gmail.com>
2015-02-12 00:21:02 +0000

    increment version

--- chz-firewall/metadata.rb
+++ chz-firewall/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "joey@cheezburger.com"
 license          "New BSD License"
 description      "Installs/Configures firewalls"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          "0.2.3"
 supports         "ubuntu"
 supports         "windows"
